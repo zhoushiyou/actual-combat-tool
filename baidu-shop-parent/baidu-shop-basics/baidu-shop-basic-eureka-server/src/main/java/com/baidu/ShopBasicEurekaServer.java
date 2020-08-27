6f3f863b1eacfb3a24913c1cd32db7eec77b2bbf
zhoushiyou <zhoushiyouuu@163.com>
2020-08-27 18:11:20 +0800

    aa

--- baidu-shop-parent/baidu-shop-basics/baidu-shop-basic-eureka-server/src/main/java/com/baidu/ShopBasicEurekaServer.java
+++ baidu-shop-parent/baidu-shop-basics/baidu-shop-basic-eureka-server/src/main/java/com/baidu/ShopBasicEurekaServer.java
@@ -16,6 +16,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ShopBasicEurekaServer {
 
     public static void main(String[] args) {
+        System.out.println("adasda");
         SpringApplication.run(ShopBasicEurekaServer.class);
     }
 }
