40f8a4e39f41949234172a2112cc5487f10b36ed
zhoushiyou <zhoushiyouuu@163.com>
2020-08-27 15:37:25 +0800

    提交项目

---
+++ baidu-shop-parent/baidu-shop-basics/baidu-shop-basic-eureka-server/src/main/java/com/baidu/ShopBasicEurekaServer.java
@@ -0,0 +1,22 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName ShopBasicEurekaServer
+ * @Description: TODO
+ * @Author zsy
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class ShopBasicEurekaServer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShopBasicEurekaServer.class);
+    }
+}
